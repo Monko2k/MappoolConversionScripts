39fb408c9cfae6e67c546a10c252dd8f40af681d
Kevin Huang <kevinhuang2k@g.ucla.edu>
2022-03-22 10:53:05 -0700

    add link handling

--- input.py
+++ input.py
@@ -8,20 +8,27 @@ cats = ("NM", "HD", "HR", "DT", "FM", "TB")
 name = input("Enter pool name: ")
 for cat in cat_title:
     lengths.append(int(input(f"Enter number of {cat} maps: ")))
+    
 
 for ind, cat in enumerate(cats):
     maps = []
     for i in range(lengths[ind]):
-        if lengths[ind] == 1:
-            mapinput = input(f"Enter {cat}: " )
-        else:
-            mapinput = input(f"Enter {cat} {i + 1}: " )
-
-        maps.append(mapinput)
-    modgroups.append({
-        "mod": cat,
-        "maps": maps
-    })
+        id = None
+        while not id:
+            if lengths[ind] == 1:
+                mapinput = input(f"Enter link or ID for {cat}: " )
+            else:
+                mapinput = input(f"Enter link or ID for {cat} {i + 1}: " )
+            id = re.search("\d+$", mapinput)
+            if id:
+                maps.append(id[0])
+            else:
+                print("Invalid beatmap link/ID")
+    if maps:
+        modgroups.append({
+            "mod": cat,
+            "maps": maps
+        })
 
 pool = {
     "name": name,
@@ -30,7 +37,8 @@ pool = {
 if not os.path.exists('./output/'):
     os.makedirs('./output/')
 with open("./output/" + name + ".json", "w+") as output: 
-    json.dump(pool, output, indent=4)
+    json.dump(pool, output, indent=2)
+    print(f"Wrote to /output/{name}.json")
 
 
 
